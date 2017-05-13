c547183da4b59a4f0f599ea8c51ef2c8ab8a3289
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2017-05-12 22:55:53 -0400

    Snapdragon groundstation packages

--- src/image_common/image_transport/tutorial/src/manifest.cpp
+++
@@ -1,8 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include <image_transport_tutorial/resized_publisher.h>
-#include <image_transport_tutorial/resized_subscriber.h>
-
-PLUGINLIB_REGISTER_CLASS(resized_pub, ResizedPublisher, image_transport::PublisherPlugin)
-
-PLUGINLIB_REGISTER_CLASS(resized_sub, ResizedSubscriber, image_transport::SubscriberPlugin)
-
